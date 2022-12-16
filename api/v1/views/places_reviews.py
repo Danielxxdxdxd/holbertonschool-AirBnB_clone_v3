4c742cbb0cfd3f334b28b0d88c07dae799ff9a30
Johanna Balcazar <106547874+LiJoBaZar@users.noreply.github.com>
2022-12-16 11:04:25 -0500

    correction output

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module for State API endpoints"""
+"""Module at state API"""
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
