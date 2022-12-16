78f6df4524557629f52a086450f77c7fd5b6dc77
Johanna Balcazar <106547874+LiJoBaZar@users.noreply.github.com>
2022-12-16 11:02:58 -0500

    Remove errors

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module for Places API endpoints"""
+"""Module at Places API"""
 from flask import jsonify, abort, request
 from api.v1.views import app_views
 from models import storage
